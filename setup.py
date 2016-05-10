3bf0bafb00ced79f659a1214d80b576ddd1fa0bc
Nipunn Koorapati <nipunn1313@gmail.com>
2016-05-10 00:57:28 -0700

    Add tox.ini

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="doorbell",
+    version="1.0",
+    description='Small app to text me when my doorbell rings',
+    license='Apache License 2.0',
+    author='Nipunn Koorapati',
+    author_email='nipunn1313@gmail.com',
+    py_modules=['doorbell'],
+    url='https://github.com/nipunn1313/doorbell',
+
+    install_requires=[],
+)
